70548bef11823acdcb076a1af4ca9276ec17acfc
Deepak Dhakad <145159816+DeepakDkd@users.noreply.github.com>
2025-05-30 16:28:38 +0530

    Refactor database connection setup and add logging in user retrieval

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -2,9 +2,24 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
-  dialect: 'postgres',
-  logging: false,
-});
+// const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+//   dialect: 'postgres',
+//   logging: false,
+// });
+const sequelize = new Sequelize('ems','postgres','deepakdkd1188@gmail.com',{
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres',
+    logging: false,
+})
 
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection established successfully.',sequelize.getDatabaseName());
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+connectDB();
 export default sequelize;
--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -4,6 +4,7 @@ import db from '../model/index';
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
     const users = await db.User.findAll();
+    console.log("//////////////////////////////////");
     res.json(users);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch users' });
