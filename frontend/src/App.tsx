0040aaed9ab358d9f2f583a0e943c46e18dd8f1f
Deepak Dhakad <145159816+DeepakDkd@users.noreply.github.com>
2025-06-22 02:44:11 +0530

    feat: add routing and components for Home page, include dependencies for React Router, Axios, and Autoprefixer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import Router from "./router";
 
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center w-full h-full bg-gray-100">
-        <div className="text-2xl">hello </div>
-      </div>
+     <Router />
     </>
   );
 }
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import Hero from "../../components/Home/Hero";
+
+function Home() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
+export default Home;
